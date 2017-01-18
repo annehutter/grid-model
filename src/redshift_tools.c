be6cf6305b4f1df706e7e900dfb83579820ae671
ahutter <anne.k.hutter@gmail.com>
2017-01-18 13:45:26 +1100

    included option to give a constant value for number of recombinations per time

---
+++ src/redshift_tools.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gsl/gsl_math.h>	//included because M_PI is not defined in <math.h>
+#include <assert.h>
+#include <complex.h>
+#include <gsl/gsl_integration.h>
+
+#ifdef __MPI
+#include <fftw3-mpi.h>
+#include <mpi.h>
+#else
+#include <fftw3.h>
+#endif
+
+#include "phys_const.h"
+#include "confObj.h"
+
+#include "redshift_tools.h"
+
+double time_from_redshift_flatuniverse(confObj_t simParam, double zmin, double zmax)
+{
+	double prefactor = 2./(3*H0*sqrt(simParam->omega_l));
+	double tmp = sqrt(simParam->omega_l/simParam->omega_m);
+	
+	return prefactor*(asinh(tmp*pow(1.+zmin, -1.5)) - asinh(tmp*pow(1.+zmax, -1.5)));
+}
---
+++ src/redshift_tools.h
@@ -0,0 +1,5 @@
+#ifndef REDSHIFT_TOOLS
+#define REDSHIFT_TOOLS
+#endif
+
+double time_from_redshift_flatuniverse(confObj_t simParam, double zmin, double zmax);
